8e1ee3effdfdce55042efb467cbb123c56a6327f
rogeraabbccdd <rogeraabbccdd@gmail.com>
2020-05-06 00:40:03 +0800

    加入 Google Analytics

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
   require_once "./config.php";
 
+  require "vendor/autoload.php";
+
+  use TheIconic\Tracking\GoogleAnalytics\Analytics;
+
   header('Content-Type: application/json; charset=utf-8');
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Headers: *');
   
+  $analytics = new Analytics(true);
+
+  $analytics
+      ->setProtocolVersion('1')
+      ->setTrackingId($gaid)
+      ->setClientId($_SERVER['REQUEST_TIME'])
+      ->setDocumentPath('/index.php')
+      ->setIpOverride($_SERVER['REMOTE_ADDR']);
+
+  $analytics->sendPageview();
+
   $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_password);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
